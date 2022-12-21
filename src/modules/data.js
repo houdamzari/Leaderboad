8192ec0bf67595ed1dca41963a725e1ec1f601b8
houdamzari <houdamzari1999@gmail.com>
2022-12-21 20:38:37 +0100

    Add Post Request

--- src/modules/data.js
+++ src/modules/data.js
@@ -1,7 +1,41 @@
+/* eslint-disable linebreak-style */
+import { baseUrl } from './env';
+import axios from 'axios';
+import { scoreDom, nameDom, submitButton } from './domSelections.js';
 export const arr = [
-  { Name: 100 },
+  {
+    Name: 'lalla',
+    score: 20,
+  },
   { Name: 20 },
   { Name: 30 },
   { Name: 40 },
   { Name: 10 },
 ];
+
+let username = '';
+let scoreInput = '';
+nameDom.addEventListener('change', (e) => {
+  submitButton.addEventListener('click', () => {
+    username = e.target.value;
+  });
+});
+scoreDom.addEventListener('change', (e) => {
+  submitButton.addEventListener('click', () => {
+    scoreInput = e.target.value;
+  });
+});
+
+const newScore = {
+  user: username,
+  score: scoreInput,
+};
+
+submitButton.addEventListener('click', async () => {
+  await axios
+    .post(
+      'https://us-central1-js-capstone-backend.cloudfunctions.net/api/](https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/OQWx7deKAZ2e0dEXiBlE/scores)',
+      newScore
+    )
+    .then((response) => console.log(response));
+});
--- src/modules/domSelections.js
+++ src/modules/domSelections.js
@@ -1,1 +1,11 @@
 export const tableDom = document.querySelector('.table');
+export const nameDom = document.querySelector(
+  '.landing__section-addScore-name'
+);
+export const scoreDom = document.querySelector(
+  '.landing__section-addScore-score'
+);
+
+export const submitButton = document.querySelector(
+  '.landing__section-addScore-button'
+);
